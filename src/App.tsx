f16670e01b452f6f6bbe8cbc41113e722af02286
bear320 <aaabear320@gmail.com>
2024-12-21 19:40:39 +0800

    feat: 🎸 Set up and configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl font-bold text-orange-500">
+      <h1 className="text-5xl font-bold text-violet-300">
         Welcome to Awwwards
       </h1>
     </main>
