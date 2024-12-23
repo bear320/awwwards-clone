b820e4b4bdaad679edcfc946854d7f76e8c62fc8
bear320 <aaabear320@gmail.com>
2024-12-23 21:03:57 +0800

    feat: 🎸 Load videos and loop infinitely

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,28 @@
+import { useRef, useState } from "react";
+
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 3;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index: number) => `videos/hero-${index}.mp4`;
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div
@@ -7,7 +31,20 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div>MiniVideoPlayer</div>
+            <div
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+              onClick={handleMiniVideoClick}
+            >
+              <video
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                src={getVideoSrc(currentIndex + 1)}
+                ref={nextVideoRef}
+                loop
+                muted
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
           </div>
         </div>
       </div>
