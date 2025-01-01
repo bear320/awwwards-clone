9027fca1e2ab16f6f733b4bfe5d928fae038c916
bear320 <aaabear320@gmail.com>
2025-01-01 14:36:53 +0800

    feat: 🎸 Create AnimatedTitle component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -31,10 +32,7 @@ const About = () => {
           Welcome to Zentry
         </h2>
 
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the world's
-          <br />l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle />
 
         <div className="about-subtext">
           <p>The Game of Games begins - your life, now an epic MMORPG</p>
