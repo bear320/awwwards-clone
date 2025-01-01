ed481cd15cad935419730a26779063cce44be26a
bear320 <aaabear320@gmail.com>
2025-01-01 14:59:18 +0800

    feat: 🎸 Add scroll animations to AnimatedTitle

--- src/components/AnimatedTitle.tsx
+++ src/components/AnimatedTitle.tsx
@@ -1,8 +1,55 @@
-const AnimatedTitle = () => {
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+const AnimatedTitle = ({
+  title,
+  containerClass,
+}: {
+  title: string;
+  containerClass?: string;
+}) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bootom",
+          toggleActions: "play none none reverse",
+        },
+      });
+
+      titleAnimation.to(".animated-word", {
+        opacity: 1,
+        transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
+        ease: "power2.inOut",
+        stagger: 0.02,
+      });
+
+      return () => ctx.revert();
+    }, containerRef);
+  }, []);
+
   return (
-    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-      Disc<b>o</b>ver the world's
-      <br />l<b>a</b>rgest shared adventure
+    <div className={`animated-title ${containerClass}`} ref={containerRef}>
+      {title.split("<br />").map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, i) => (
+            <span
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{
+                __html: word,
+              }}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
